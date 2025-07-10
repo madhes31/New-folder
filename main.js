1283f1ebbc5fe3e5ad246d5fab7b4851d3482049
madheswaran u <madhesw020@gmail.com>
2025-07-10 12:05:35 +0530

    added

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed('.text', {
+    strings: ['Electrical and Electronics Engineering Student', 'Problem Solver', 'Time Manager', 'AutoCAD Enthusiast',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
